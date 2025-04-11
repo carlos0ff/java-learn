3349ed5b1b301617e3acf8b73e9f37941be1aca7
carlos0ff <carlosiilva66@gmail.com>
2025-04-11 14:34:21 -0300

    feat: add Java control flow examples (if-else, switch, for/while loops)

--- src/br/com/javalearn/basics/ControlFlow.java
+++ src/br/com/javalearn/basics/ControlFlow.java
@@ -1,29 +1,43 @@
 package br.com.javalearn.basics;
 
-import java.time.Duration;
-import java.time.LocalTime;
 
 
 public class ControlFlow {
-
-    public static String diferencaHoras(String hora1, String hora2) 
-    {
-        LocalTime h1 = LocalTime.parse(hora1);
-        LocalTime h2 = LocalTime.parse(hora2);
-        
-        Duration duracao = Duration.between(h1, h2);
-
-        long horas = duracao.toHours();
-        long minutos = duracao.toMinutes() % 60;
-        
-        return String.format("%02d horas e %02d minutos", Math.abs(horas), Math.abs(minutos));
-    }
-
     public static void main(String[] args) {
+        // if-else 
+        int score = 85;
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else {
+            System.out.println("Grade: C or below");
+        }
         
-        System.out.println(diferencaHoras("06:00:00", "15:48:00")); 
-        System.out.println(diferencaHoras("01:09:00", "06:00:00")); 
+        // switch 
+        String day = "Monday";
+        switch (day) {
+            case "Monday":
+                System.out.println("Start of work week");
+                break;
+            case "Friday":
+                System.out.println("End of work week");
+                break;
+            default:
+                System.out.println("Midweek day");
+        }
+        
+        // for loop 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+        
+        // while loop 
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+        
     }
-
-   
 }
