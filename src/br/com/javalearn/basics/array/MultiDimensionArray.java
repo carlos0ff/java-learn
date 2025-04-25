e770d97394e749bc65b3d1aef098651fbd2b1255
carlos0ff <carlosiilva66@gmail.com>
2025-04-25 11:30:19 -0300

    Added MultiDimensionArray class to demonstrate multi-dimensional arrays

--- src/br/com/javalearn/basics/array/MultiDimensionArray.java
+++ src/br/com/javalearn/basics/array/MultiDimensionArray.java
@@ -1,5 +1,13 @@
 package br.com.javalearn.basics.array;
 
+/**
+ * Demonstrates usage of multi‐dimensional arrays in Java.
+ * In Java, a 2D array is really an "array of arrays," which
+ * allows both rectangular and jagged (varying-length rows) structures.
+ */
 public class MultiDimensionArray {
     
+    public static void main(String[] args) {
+        
+    }
 }
